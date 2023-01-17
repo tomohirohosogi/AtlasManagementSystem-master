2dfcdb4be71e118b597135c2a2e2f60774389839
tomohirohosogi <117811931+tomohirohosogi@users.noreply.github.com>
2023-01-17 22:30:09 +0900

    Add files via upload

---
+++ database/seeds/SubjectsTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Users\Subjects;
+
+class SubjectsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // 国語、数学、英語を追加
+        DB::table('subjects')->insert([
+            'subject'=>'国語',
+        ]);
+
+                DB::table('subjects')->insert([
+            'subject'=>'数学',
+        ]);
+
+                DB::table('subjects')->insert([
+            'subject'=>'英語',
+        ]);
+    }
+}
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Users\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'over_name' => '細木',
+            'under_name' => '智広',
+            'over_name_kana' => 'ホソギ',
+            'under_name_kana' => 'トモヒロ',
+            'mail_address' => '1111@1111',
+            'sex' => '1',
+            'birth_day' => '19990101',
+            'role' => '1',
+            'password' => '11111111',
+        ]);
+
+    }
+}
